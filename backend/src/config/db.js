c44adcf6837bfac5fa5d7e06b08721eef99443a3
manu92ndro <112045103+manu92ndro@users.noreply.github.com>
2026-07-18 07:40:33 -0500

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -14,11 +14,11 @@ console.log("PASSWORD START:", process.env.DB_PASSWORD.substring(0,2));
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 3306,
+  host: localhost,
+  user: u800553857_andro,
+  password: MAnustchj92$c,
+  database: u800553857_digithex,
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
