06cc6e27c48dceca5a97f9839a3ea18d04cc3da6
manu92ndro <112045103+manu92ndro@users.noreply.github.com>
2026-07-20 12:52:26 -0500

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,11 @@
 
 require('dotenv').config();
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", JSON.stringify(process.env.DB_PASSWORD));
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 console.log("DB CONFIG:", {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
