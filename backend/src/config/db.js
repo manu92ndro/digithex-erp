caf119683ad8aa714a6278e5bf9fd84958eca646
manu92ndro <domi92.andro@gmail.com>
2026-07-21 10:45:45 -0500

    cambio

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,16 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 3306,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
+
+(async () => {
+  try {
+    const conn = await pool.getConnection();
+    console.log("✅ MySQL conectado correctamente");
+    conn.release();
+  } catch (err) {
+    console.error("❌ Error de conexión MySQL");
+    console.error(err);
+  }
+})();
 
 module.exports = pool;
